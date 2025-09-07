9badcac61e6e5ed0e2220af40ce65a2ad75e92d2
B MAIBU <bmaibu213@gmail.com>
2025-09-07 22:33:51 +0530

    Update skills.js

--- skills.js
+++ skills.js
@@ -1,24 +1,31 @@
+
 function animateBar(id, percent) {
-    const bar = document.getElementById(id);
-    let current = 0;
+  const bar = document.getElementById(id);
+  let current = 0;
 
-    const interval = setInterval(() => {
-      if (current >= percent) {
-        clearInterval(interval);
-      } else {
-        current++;
-        bar.style.width = current + '%';
-        bar.textContent = current + '%';
-      }
-    }, 20);
-  }
+  const interval = setInterval(() => {
+    if (current >= percent) {
+      clearInterval(interval);
+    } else {
+      current++;
+      bar.style.width = current + '%';
+      bar.textContent = current + '%';
+    }
+  }, 20);
+}
 
-  // Animate all skill bars on load
-  window.onload = () => {
-    animateBar("html-bar", 80);
-    animateBar("css-bar", 70);
-    animateBar("js-bar", 60);
-    animateBar("react-bar", 70);
-    animateBar("cpp-bar", 60);
-    animateBar("dsa-bar", 50);
-  };
+// Animate all skill bars on load
+window.onload = () => {
+  animateBar("html-bar", 80);
+  animateBar("css-bar", 70);
+  animateBar("js-bar", 65);
+  animateBar("mg-bar", 60);       
+  animateBar("ex-bar", 70);       
+  animateBar("react-bar", 70);    
+  animateBar("nd-bar", 70);       
+  animateBar("cpp-bar", 60);      
+  animateBar("dsa-bar", 50);      
+  animateBar("oop-bar", 60);      
+  animateBar("gh-bar", 70);       
+};
+
